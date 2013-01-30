2e0a99dfe28b478daf239cf5bb6ea75c763a50da
Ubuntu <ubuntu@ip-10-117-33-78.ec2.internal>
2013-01-30 21:47:30 +0000

    init

---
+++ public_html/var/autoload/ezp_extension.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Autoloader definition for eZ Publish Extension files.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version  2012.8
+ * @package kernel
+ *
+ */
+
+return array(
+      'EnchantSpell'                        => 'extension/ezoe/modules/ezoe/classes/EnchantSpell.php',
+      'GoogleSpell'                         => 'extension/ezoe/modules/ezoe/classes/GoogleSpell.php',
+      'Moxiecode_JSON'                      => 'extension/ezoe/modules/ezoe/classes/utils/mcejson.php',
+      'Moxiecode_JSONReader'                => 'extension/ezoe/modules/ezoe/classes/utils/mcejson.php',
+      'PSpell'                              => 'extension/ezoe/modules/ezoe/classes/PSpell.php',
+      'PSpellShell'                         => 'extension/ezoe/modules/ezoe/classes/PSpellShell.php',
+      'SpellChecker'                        => 'extension/ezoe/modules/ezoe/classes/SpellChecker.php',
+      'eZArchive'                           => 'extension/ezwebin/autoloads/ezarchive.php',
+      'eZIEEzcConversions'                  => 'extension/ezie/classes/interfaces/conversions.php',
+      'eZIEEzcGDHandler'                    => 'extension/ezie/classes/handlers/gd.php',
+      'eZIEEzcImageMagickHandler'           => 'extension/ezie/classes/handlers/magick.php',
+      'eZIEImageAction'                     => 'extension/ezie/classes/image_action.php',
+      'eZIEImageAnalyzer'                   => 'extension/ezie/classes/image_analyzer.php',
+      'eZIEImageFilterBW'                   => 'extension/ezie/classes/image_filter_bw.php',
+      'eZIEImageFilterBrightness'           => 'extension/ezie/classes/image_filter_brightness.php',
+      'eZIEImageFilterContrast'             => 'extension/ezie/classes/image_filter_contrast.php',
+      'eZIEImageFilterSepia'                => 'extension/ezie/classes/image_filter_sepia.php',
+      'eZIEImagePreAction'                  => 'extension/ezie/classes/image_pre_action.php',
+      'eZIEImageToolCrop'                   => 'extension/ezie/classes/image_tool_crop.php',
+      'eZIEImageToolFlipHorizontally'       => 'extension/ezie/classes/image_tool_flip_horizontally.php',
+      'eZIEImageToolFlipVertically'         => 'extension/ezie/classes/image_tool_flip_vertically.php',
+      'eZIEImageToolPixelate'               => 'extension/ezie/classes/image_tool_pixelate.php',
+      'eZIEImageToolResize'                 => 'extension/ezie/classes/image_tool_resize.php',
+      'eZIEImageToolRotation'               => 'extension/ezie/classes/image_tool_rotation.php',
+      'eZIEImageToolWatermark'              => 'extension/ezie/classes/image_tool_watermark.php',
+      'eZIEezcImageConverter'               => 'extension/ezie/classes/image_converter.php',
+      'eZKeywordList'                       => 'extension/ezwebin/autoloads/ezkeywordlist.php',
+      'eZOEInputParser'                     => 'extension/ezoe/ezxmltext/handlers/input/ezoeinputparser.php',
+      'eZOETemplateUtils'                   => 'extension/ezoe/autoloads/ezoetemplateutils.php',
+      'eZOEXMLInput'                        => 'extension/ezoe/ezxmltext/handlers/input/ezoexmlinput.php',
+      'eZPageData'                          => 'extension/ezwebin/autoloads/ezpagedata.php',
+      'eZTagCloud'                          => 'extension/ezwebin/autoloads/eztagcloud.php',
+      'ezjscAccessTemplateFunctions'        => 'extension/ezjscore/autoloads/ezjscaccesstemplatefunctions.php',
+      'ezjscAjaxContent'                    => 'extension/ezjscore/classes/ezjscajaxcontent.php',
+      'ezjscCssOptimizer'                   => 'extension/ezjscore/classes/ezjsccssoptimizer.php',
+      'ezjscEncodingTemplateFunctions'      => 'extension/ezjscore/autoloads/ezjscencodingtemplatefunctions.php',
+      'ezjscJavascriptOptimizer'            => 'extension/ezjscore/classes/ezjscjavascriptoptimizer.php',
+      'ezjscPacker'                         => 'extension/ezjscore/classes/ezjscpacker.php',
+      'ezjscPackerTemplateFunctions'        => 'extension/ezjscore/autoloads/ezjscpackertemplatefunctions.php',
+      'ezjscServerFunctions'                => 'extension/ezjscore/classes/ezjscserverfunctions.php',
+      'ezjscServerFunctionsAjaxUploader'    => 'extension/ezjscore/classes/ezjscserverfunctionsajaxuploader.php',
+      'ezjscServerFunctionsAutosave'        => 'extension/ezautosave/classes/ezjscserverfunctionsautosave.php',
+      'ezjscServerFunctionsJs'              => 'extension/ezjscore/classes/ezjscserverfunctionsjs.php',
+      'ezjscServerFunctionsNode'            => 'extension/ezjscore/classes/ezjscserverfunctionsnode.php',
+      'ezjscServerFunctionsPublishingQueue' => 'extension/ezjscore/classes/ezjscserverfunctionspublishingqueue.php',
+      'ezjscServerRouter'                   => 'extension/ezjscore/classes/ezjscserverrouter.php',
+      'ezoeServerFunctions'                 => 'extension/ezoe/classes/ezoeserverfunctions.php',
+      'ezpAjaxUploaderHandlerInterface'     => 'extension/ezjscore/classes/ajaxuploader/ezpajaxuploaderhandlerinterface.php',
+      'ezpRelationAjaxUploader'             => 'extension/ezjscore/classes/ajaxuploader/ezprelationajaxuploader.php',
+      'ezpRelationListAjaxUploader'         => 'extension/ezjscore/classes/ajaxuploader/ezprelationlistajaxuploader.php',
+      'ezwebinInfo'                         => 'extension/ezwebin/ezinfo.php',
+    );
+
+?>
